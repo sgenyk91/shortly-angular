7384ca34c7ef3d814f0b3978bafe052b9edf2130
sgenyk91 <sgenyk91@gmail.com>
2014-11-25 19:08:43 -0800

    Implements validation

--- client/app/app.js
+++ client/app/app.js
@@ -28,7 +28,8 @@ angular.module('shortly', [
     .when('/shorten', {
       templateUrl: 'app/shorten/shorten.html',
       controller: 'ShortenController'
-    });
+    })
+    .otherwise({ redirectTo: '/links'});
     // We add our $httpInterceptor into the array
     // of interceptors. Think of it like middleware for your ajax calls
     $httpProvider.interceptors.push('AttachTokens');
